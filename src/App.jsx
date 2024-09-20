f529ae500285f607bcd55d00934d8704df873647
Gourav <mrgoravsainimrt@gmail.com>
2024-09-20 23:28:23 +0530

    comm

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useState, useEffect } from 'react'
 import { FaSearch } from "react-icons/fa";
-import { BsSunriseFill } from "react-icons/bs";
-import { BsSunset } from "react-icons/bs";
-import { IoMoonSharp } from "react-icons/io5";
-import { WiMoonAltNew } from "react-icons/wi";
+import { FaLocationCrosshairs } from "react-icons/fa6";
+import { PiSunHorizonFill } from "react-icons/pi";
+import { FaMoon } from "react-icons/fa";
 
 
 function App() {
@@ -14,13 +13,13 @@ function App() {
   const [day2, setDay2] = useState();
   const [day3, setDay3] = useState();
   const [forecast, setForecast] = useState();
-  const [search, setSearch] = useState("Meerut");
+  const [search, setSearch] = useState("");
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const fetchApi = async () => {
-      const url = `https://api.weatherapi.com/v1/forecast.json?key=65036d46f3ba4ddaa7a80846241201&q=${search}&days=3&aqi=yes&alerts=no`
+      const url = `https://api.weatherapi.com/v1/forecast.json?key=65036d46f3ba4ddaa7a80846241201&q=meerut&days=3&aqi=yes&alerts=no`
       const response = await fetch(url);
       const jsondata = await response.json();
       // console.log(jsondata);
@@ -54,12 +53,12 @@ function App() {
   const formattedDate = formatDate(lastUpdated);
 
   const handleSubmit = async () => {
-    
+
     const url = `https://api.weatherapi.com/v1/forecast.json?key=65036d46f3ba4ddaa7a80846241201&q=${search}&days=7&aqi=yes&alerts=no`
     const response = await fetch(url);
     if (!response.ok) {
       setError("place not found");
-     
+
     }
     if (response.ok) {
       const jsondata = await response.json();
@@ -67,10 +66,33 @@ function App() {
       setLocation(jsondata.location);
       setForecast(jsondata.forecast && jsondata.forecast.forecastday[0]);
       setHour(jsondata.forecast && jsondata.forecast.forecastday[0] && jsondata.forecast.forecastday[0].hour)
-      
+      setSearch("")
       // console.log(jsondata)
-      
+
       setError("")
+    }
+  }
+
+  const getLocation = async (lat, lng) => {
+    // console.log('request sent to server')
+    const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json`;
+
+    const response = await fetch(url);
+    // console.log(response);
+    // console.log("saprate........................");
+    const data = await response.json();
+    // console.log(data.address.city);
+    setSearch(data && data.address && data.address.city);
+    // handleSubmit();
+  };
+
+
+  const handleLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        getLocation(latitude, longitude);
+      })
     }
   }
 
@@ -79,7 +101,7 @@ function App() {
   return (
     <>
       <div className='w-screen h-screen flex justify-center'>
-        <div className='bg-sky-500 h-screen max-w-[430px] overflow-y-auto'>
+        <div className='bg-sky-300 h-screen max-w-[430px] overflow-y-auto'>
           <div className='max-w-[360px]'>
             {loading ? (
               <div className='text-center text-white mt-10 max-w-[360px]'>Loading...</div>
@@ -87,7 +109,8 @@ function App() {
               <>
                 <div className='flex justify-between m-3 mt-3'>
                   <div className='w-10/12'><input type="search" value={search} onChange={(e) => setSearch(e.target.value)} placeholder='search by city' className='p-1 rounded-lg w-full' /></div>
-                  <div className='text-center w-2/12 flex justify-end'><FaSearch onClick={handleSubmit} size={25} className='mt-1 mr-2 cursor-pointer' />
+                  <div className='text-center w-2/12 flex justify-between ml-1'><FaSearch onClick={handleSubmit} size={25} className='mt-1 mr-2 cursor-pointer' />
+                    <FaLocationCrosshairs size={25} className='mt-1 mr-2 cursor-pointer' onClick={handleLocation}/>
                   </div>
                 </div>
                 {
@@ -100,8 +123,8 @@ function App() {
                 </div>
                 <div className='flex justify-around mt-3'>
                   <div className=''>
-                    <p className='font-bold text-5xl'>{current && current.temp_c}&deg;c</p>
-                    <p>{formattedDate}</p>
+                    <p className='font-bold text-5xl text-gray-800'>{current && current.temp_c}&deg;c</p>
+                    <p className='text-black'>{formattedDate}</p>
                   </div>
                   <div className=''>
                     <img src={current && current.condition && current.condition.icon} alt="Weather Icon" className="w-20" />
@@ -109,10 +132,10 @@ function App() {
                   </div>
                 </div>
                 <div>
-                  <p className='mt-2 p-2 font-semibold ml-3 bg-sky-600 rounded-xl mr-3'>min temp- {forecast && forecast.day && forecast.day.mintemp_c}&deg;c / max temp- {forecast && forecast.day && forecast.day.maxtemp_c}&deg;c</p>
+                  <p className='mt-2 p-2 font-semibold ml-3 bg-sky-400 text-gray-100 rounded-xl mr-3'>min temp- {forecast && forecast.day && forecast.day.mintemp_c}&deg;c / max temp- {forecast && forecast.day && forecast.day.maxtemp_c}&deg;c</p>
                 </div>
                 <div className='w-full'>
-                  <div className='bg-sky-600 h-auto w-11/12 mx-auto m-2 rounded-2xl'>
+                  <div className='bg-sky-400 h-auto w-11/12 mx-auto m-2 rounded-2xl'>
                     <div className='ml-2 mt-3 pt-3 pb-3'>
                       <p className='text-white text-xl'>{current && current.condition && current.condition.text}, low {forecast && forecast.day && forecast.day.mintemp_c}&deg;c</p>
                     </div>
@@ -127,7 +150,7 @@ function App() {
                             <li key={index} className='w-16 h-auto flex flex-col items-center'> {/* Changed to <li> */}
                               <p className='block p-2 text-white'>{formattedTime}</p>
                               <p className='block p-2'><img src={hour.condition?.icon} alt={hour.condition?.text} className="w-20 " /></p>
-                              <p className='block p-2 text-gray-300'>{hour.temp_c}&deg;c</p>
+                              <p className='block p-2 text-gray-100'>{hour.temp_c}&deg;c</p>
                             </li>
                           );
                         })}
@@ -136,25 +159,25 @@ function App() {
                   </div>
                 </div>
                 <div className='w-full mt-2'>
-                  <div className='bg-sky-600 h-auto w-11/12 mx-auto my-2 rounded-2xl'>
+                  <div className='bg-sky-400 h-auto w-11/12 mx-auto my-2 rounded-2xl'>
                     <div className='p-4'>
                       <div className='flex flex-row justify-between items-center'>
                         <h3 className='font-bold text-xl text-gray-300'>Today</h3>
-                        <p className='text-lg ml-2'>{day1 && day1.day && day1.day.maxtemp_c}/{day1 && day1.day && day1.day.mintemp_c}°C</p>
+                        <p className='text-lg ml-2 text-gray-100'>{day1 && day1.day && day1.day.maxtemp_c}/{day1 && day1.day && day1.day.mintemp_c}°C</p>
                         <div className='flex space-x-1 ml-1'>
                           <img src={day1 && day1.day && day1.day.condition && day1.day.condition.icon} alt="weather icon" className='w-12 h-12' />
                         </div>
                       </div>
                       <div className='flex flex-row justify-between items-center'>
                         <h3 className='font-bold text-xl text-white'>{getDayFromEpoch(day2 && day2.date_epoch)}</h3>
-                        <p className='text-lg ml-2'>{day2 && day2.day && day2.day.maxtemp_c}/{day2 && day2.day && day2.day.mintemp_c}°C</p>
+                        <p className='text-lg ml-2 text-gray-100'>{day2 && day2.day && day2.day.maxtemp_c}/{day2 && day2.day && day2.day.mintemp_c}°C</p>
                         <div className='flex space-x-1 ml-1'>
                           <img src={day2 && day2.day && day2.day.condition && day2.day.condition.icon} alt="weather icon" className='w-12 h-12' />
                         </div>
                       </div>
                       <div className='flex flex-row justify-between items-center'>
                         <h3 className='font-bold text-xl text-white'>{getDayFromEpoch(day3 && day3.date_epoch)}</h3>
-                        <p className='text-lg ml-2'>{day3 && day3.day && day3.day.maxtemp_c}/{day3 && day3.day && day3.day.mintemp_c}°C</p>
+                        <p className='text-lg ml-2 text-gray-100'>{day3 && day3.day && day3.day.maxtemp_c}/{day3 && day3.day && day3.day.mintemp_c}°C</p>
                         <div className='flex space-x-1 ml-1'>
                           <img src={day3 && day3.day && day3.day.condition && day3.day.condition.icon} alt="weather icon" className='w-12 h-12' />
                         </div>
@@ -163,9 +186,9 @@ function App() {
                   </div>
                 </div>
                 <div className='w-full mt-2'>
-                  <div className='bg-sky-600 h-auto w-11/12 mx-auto my-2 rounded-2xl'>
+                  <div className='bg-sky-400 h-auto w-11/12 mx-auto my-2 rounded-2xl'>
                     <div className='p-4 flex flex-col'>
-                      <p className='text-center text-2xl font-semibold text-gray-300'>AQI Level</p>
+                      <p className='text-center text-2xl font-semibold text-gray-200'>AQI Level</p>
                       <p className='text-center text-2xl font-semibold text-white'>{current && current.air_quality && current.air_quality.o3}</p>
                     </div>
                   </div>
@@ -174,32 +197,32 @@ function App() {
                 <div className='w-full mt-2'>
                   <div className='h-auto w-11/12 mx-auto my-2 rounded-2xl space-y-2'>
                     <div className='flex flex-row space-x-3 justify-around rounded-2xl'>
-                      <div className='bg-sky-600 h-28 w-32 p-2 rounded-2xl'>
-                        <p className='text-center text-gray-300'>Apparent Temperature</p>
+                      <div className='bg-sky-400 h-28 w-32 p-2 rounded-2xl'>
+                        <p className='text-center text-gray-200'>Apparent Temperature</p>
                         <p className='text-center text-white mt-3 text-2xl'>{current && current.temp_c}&deg;c</p>
                       </div>
-                      <div className='bg-sky-600 h-28 w-32 p-2 rounded-2xl'>
-                        <p className='text-center text-gray-300'>Humidity</p>
+                      <div className='bg-sky-400 h-28 w-32 p-2 rounded-2xl'>
+                        <p className='text-center text-gray-200'>Humidity</p>
                         <p className='text-center text-white mt-3 text-2xl'>{current && current.humidity}%</p>
                       </div>
                     </div>
                     <div className='flex flex-row space-x-3 justify-around rounded-2xl'>
-                      <div className='bg-sky-600 h-28 w-32 p-2 rounded-2xl'>
-                        <p className='text-center text-gray-300'>Wind speed</p>
+                      <div className='bg-sky-400 h-28 w-32 p-2 rounded-2xl'>
+                        <p className='text-center text-gray-200'>Wind speed</p>
                         <p className='text-center text-white mt-3 text-2xl'>{current && current.wind_kph} K/ph</p>
                       </div>
-                      <div className='bg-sky-600 h-28 w-32 p-2 rounded-2xl'>
-                        <p className='text-center text-gray-300'>UV</p>
+                      <div className='bg-sky-400 h-28 w-32 p-2 rounded-2xl'>
+                        <p className='text-center text-gray-200'>UV</p>
                         <p className='text-center text-white mt-3 text-2xl'>{current && current.uv}</p>
                       </div>
                     </div>
                     <div className='flex flex-row space-x-3 justify-around rounded-2xl'>
-                      <div className='bg-sky-600 h-28 w-32 p-2 rounded-2xl'>
-                        <p className='text-center text-gray-300'>visibility</p>
+                      <div className='bg-sky-400 h-28 w-32 p-2 rounded-2xl'>
+                        <p className='text-center text-gray-200'>visibility</p>
                         <p className='text-center text-white mt-3 text-2xl'>{current && current.vis_km} Km</p>
                       </div>
-                      <div className='bg-sky-600 h-28 w-32 p-2 rounded-2xl'>
-                        <p className='text-center text-gray-300'>Air pressure</p>
+                      <div className='bg-sky-400 h-28 w-32 p-2 rounded-2xl'>
+                        <p className='text-center text-gray-200'>Air pressure</p>
                         <p className='text-center text-white mt-3 text-2xl'>{current && current.pressure_mb} hpa</p>
                       </div>
                     </div>
@@ -207,14 +230,12 @@ function App() {
                 </div>
 
                 <div className='w-full mt-2'>
-                  <div className='bg-sky-600 h-auto w-11/12 mx-auto my-2 rounded-2xl'>
+                  <div className='bg-sky-400 h-auto w-11/12 mx-auto my-2 rounded-2xl'>
                     <div className='flex flex-row space-x-3 justify-around'>
-                      <div className='flex flex-col space-y-2 mb-2'>
-                        <p className='text-center mt-3 ml-7'><BsSunriseFill size={30} /></p>
+                      <div className='flex flex-row space-x-2 mb-2 items-center'>
                         <p className='text-white text-xl m-2'>{forecast && forecast.astro && forecast.astro.sunrise}</p>
-                      </div>
-                      <div className='flex flex-col space-y-2 mb-2'>
-                        <p className='text-center mt-3 ml-7'><BsSunset size={30} /></p>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwU313zmtxd0V5QBkb8XiaQoK9gtRAQ-AIeA&s" alt="" height={130} width={130} className='mt-2'/>
+                        {/* <PiSunHorizonFill size={100} color='red' className='mt-2'/> */}
                         <p className='text-white text-xl m-2'>{forecast && forecast.astro && forecast.astro.sunset}</p>
                       </div>
                     </div>
@@ -222,14 +243,12 @@ function App() {
                 </div>
 
                 <div className='w-full mt-2'>
-                  <div className='bg-sky-600 h-auto w-11/12 mx-auto my-2 rounded-2xl'>
+                  <div className='bg-sky-400 h-auto w-11/12 mx-auto my-2 rounded-2xl'>
                     <div className='flex flex-row space-x-3 justify-around'>
-                      <div className='flex flex-col space-y-2 mb-2'>
-                        <p className='text-center mt-3 ml-7'><IoMoonSharp size={30} /></p>
+                      <div className='flex flex-row space-x-2 mb-2 items-center'>
                         <p className='text-white text-xl m-2'>{forecast && forecast.astro && forecast.astro.moonrise}</p>
-                      </div>
-                      <div className='flex flex-col space-y-2 mb-2'>
-                        <p className='text-center mt-3 ml-7'><WiMoonAltNew size={30} /></p>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNWjLdkZEH2qnf0sfoDtKBR9z3Yv-kY6Xg1Q&s" alt="" height={130} width={130} className='mt-2'/>
+                        {/* <FaMoon size={100} color='red' className='mt-2'/> */}
                         <p className='text-white text-xl m-2'>{forecast && forecast.astro && forecast.astro.moonset}</p>
                       </div>
                     </div>
@@ -247,23 +266,3 @@ function App() {
 }
 
 export default App
-
-
-// <ul className='space-y-4'>
-//                         {day1 && day1.length > 0 && day1.map((day, index) => {
-//                           const maxTemp = day && day.day && day.day.maxtemp_c; // Replace with actual max temp value
-//                           const minTemp = day && day.day && day.day.mintemp_c; // Replace with actual min temp value
-//                           const dayName = getDayFromEpoch(day && day.date_epoch); // Use your day extraction function
-//                           return (
-//                             <li key={index} className='flex-none h-16 bg-white rounded-lg shadow-md p-2'>
-//                               <div className='flex flex-row justify-between items-center'>
-//                                 <h3 className='font-bold text-xl'>{dayName}</h3>
-//                                 <p className='text-lg ml-2'>{maxTemp}/{minTemp}°C</p>
-//                                 <div className='flex space-x-1 ml-1'>
-//                                   <img src={day && day.day && day.day.condition && day.day.condition.icon} alt="weather icon" className='w-12 h-12' />
-//                                 </div>
-//                               </div>
-//                             </li>
-//                           );
-//                         })}
-//                       </ul>
